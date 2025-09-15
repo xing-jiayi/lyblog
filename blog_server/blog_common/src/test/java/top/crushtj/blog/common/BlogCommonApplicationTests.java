505d313cf1d276e7900a4265b9dcb0a2d301eed4
ayi <2294931964@qq.com>
2025-09-15 16:10:04 +0800

    后端初始化

---
+++ blog_server/blog_common/src/test/java/top/crushtj/blog/common/BlogCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package top.crushtj.blog.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ blog_server/blog_web/src/test/java/top/crushtj/blog/web/BlogWebApplicationTests.java
@@ -0,0 +1,13 @@
+package top.crushtj.blog.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
