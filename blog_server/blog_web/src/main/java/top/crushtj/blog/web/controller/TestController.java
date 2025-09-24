7883ad32f32d8f06d012922e8f596346ce477481
ayi <2294931964@qq.com>
2025-09-24 10:40:07 +0800

    参数校验简单实现及测试

--- blog_server/blog_web/src/main/java/top/crushtj/blog/web/controller/TestController.java
+++ blog_server/blog_web/src/main/java/top/crushtj/blog/web/controller/TestController.java
@@ -2,11 +2,17 @@ package top.crushtj.blog.web.controller;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import top.crushtj.blog.common.aspect.ApiOperationLog;
 import top.crushtj.blog.web.model.User;
+
+import java.util.stream.Collectors;
 
 /**
  * @author 刑加一
@@ -26,5 +32,23 @@ public class TestController {
         log.info("232323");
         log.debug("232323");
         return user;
+    }
+
+    @PostMapping("/testValida")
+    @ApiOperationLog(description = "测试接口")
+    public ResponseEntity<String> test(@RequestBody @Validated User user, BindingResult bindingResult) {
+        // 是否存在校验错误
+        if (bindingResult.hasErrors()) {
+            // 获取校验不通过字段的提示信息
+            String errorMsg = bindingResult.getFieldErrors()
+                    .stream()
+                    .map(FieldError::getDefaultMessage)
+                    .collect(Collectors.joining(", "));
+
+            return ResponseEntity.badRequest().body(errorMsg);
+        }
+
+        // 返参
+        return ResponseEntity.ok("参数没有任何问题");
     }
 }
--- blog_server/blog_web/src/main/java/top/crushtj/blog/web/model/User.java
+++ blog_server/blog_web/src/main/java/top/crushtj/blog/web/model/User.java
@@ -3,6 +3,8 @@ package top.crushtj.blog.web.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.*;
+
 /**
  * @author 刑加一
  * @url www.crushtj.top
@@ -15,10 +17,27 @@ public class User {
     /**
      * 用户名
      */
+    @NotBlank(message = "用户名不能为空")
     private String username;
 
     /**
      * 性别，0-未知，1-男，2-女
      */
+    @NotNull(message = "性别不能为空")
     private Integer gender;
+
+    /**
+     * 年龄
+     */
+    @NotNull(message = "年龄不能为空")
+    @Min(value = 18, message = "年龄必须大于等于18岁")
+    @Max(value = 120, message = "年龄必须小于等于120岁")
+    private Integer age;
+
+    /**
+     * 邮箱
+     */
+    @NotBlank(message = "邮箱不能为空")
+    @Email(message = "邮箱格式不正确")
+    private String email;
 }
