4c9657a7837735c38495a40bee52a85f00e11ed4
ayi <2294931964@qq.com>
2025-09-28 23:43:29 +0800

    绘制登录页面

--- lyblog_web/src/main.js
+++ lyblog_web/src/main.js
@@ -3,5 +3,10 @@ import '@/assets/main.css'
 import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
   
 createApp(App).use(router).mount('#app')
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
