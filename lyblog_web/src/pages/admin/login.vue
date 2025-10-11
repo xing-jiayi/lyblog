0865d09fee8019e30c9d70aa1757156ca72648aa
ayi <2294931964@qq.com>
2025-10-11 18:26:01 +0800

    实现接口鉴权

--- lyblog_web/src/pages/admin/login.vue
+++ lyblog_web/src/pages/admin/login.vue
@@ -2,7 +2,8 @@
 	<div class="grid grid-cols-2 h-screen">
 		<div class="col-span-2 order-2 p-10 md:col-span-1 md:order-1 bg-slate-900">
 			<!-- 指定为 flex 布局，并设置为屏幕垂直水平居中，高度为 100% -->
-			<div class="flex justify-center items-center h-full flex-col animate__animated animate__bounceInLeft animate__fast">
+			<div
+				class="flex justify-center items-center h-full flex-col animate__animated animate__bounceInLeft animate__fast">
 				<h2 class="font-bold text-4xl mb-7 text-white">LyBlog 博客登录</h2>
 				<p class="text-white">一款由 Spring Boot + Mybaits Plus + Vue 3.2 + Vite 4 开发的前后端分离博客。</p>
 				<!-- 指定图片宽度为父级元素的 1/2 -->
@@ -11,12 +12,13 @@
 		</div>
 		<div class="col-span-2 order-1 md:col-span-1 md:order-2 bg-white">
 			<!-- flex-col 用于指定子元素垂直排列 -->
-			<div class="flex justify-center items-center h-full flex-col animate__animated animate__bounceInRight animate__fast">
+			<div
+				class="flex justify-center items-center h-full flex-col animate__animated animate__bounceInRight animate__fast">
 				<h1 class="font-bold text-4xl mb-5">欢迎回来</h1>
 				<!-- 设置 flex 布局，内容垂直水平居中，文字颜色，以及子内容水平方向 x 轴间距 -->
 				<div class="flex items-center justify-center mb-7 text-gray-400 space-x-2">
 					<!-- 左边横线，高度为 1px, 宽度为 16，背景色设置 -->
-					<span class="h-[1px] w-16 bg-gray-200"></span>  
+					<span class="h-[1px] w-16 bg-gray-200"></span>
 					<span>账号密码登录</span>
 					<!-- 右边横线 -->
 					<span class="h-[1px] w-16 bg-gray-200"></span>
@@ -25,15 +27,21 @@
 				<el-form class="w-5/6 md:w-2/5">
 					<el-form-item>
 						<!-- 输入框组件 -->
-						<el-input size="large" placeholder="请输入用户名" :prefix-icon="User" clearable />
+						<el-input size="large" v-model="user.username" placeholder="请输入用户名" :prefix-icon="User" clearable />
 					</el-form-item>
 					<el-form-item>
 						<!-- 密码框组件 -->
-						<el-input size="large" type="password" placeholder="请输入密码" :prefix-icon="Lock" clearable />
+						<el-input
+							size="large"
+							v-model="user.password"
+							type="password"
+							placeholder="请输入密码"
+							:prefix-icon="Lock"
+							clearable />
 					</el-form-item>
 					<el-form-item>
 						<!-- 登录按钮，宽度设置为 100% -->
-						<el-button class="w-full" size="large" type="primary">登录</el-button>
+						<el-button class="w-full" size="large" type="primary" @click="onSubmit">登录</el-button>
 					</el-form-item>
 				</el-form>
 			</div>
@@ -44,4 +52,26 @@
 <script setup>
 	// 引入 Element Plus 中的用户、锁图标
 	import { User, Lock } from "@element-plus/icons-vue"
+	import { login } from "@/api/admin/user"
+	import { reactive } from "vue"
+	import { useRouter } from "vue-router"
+
+	const router = useRouter()
+
+	const user = reactive({
+		username: "",
+		password: "",
+	})
+
+	const onSubmit = () => {
+		console.log("登录")
+		login(user.username, user.password).then((res) => {
+			console.log(res)
+			// 判断是否成功
+			if (res.data.success == true) {
+				// 跳转到后台首页
+				router.push("/admin/index")
+			}
+		})
+	}
 </script>
--- lyblog_web/src/router/index.js
+++ lyblog_web/src/router/index.js
@@ -1,31 +1,40 @@
-import Index from "@/pages/frontend/index.vue";
-import { createRouter, createWebHashHistory } from 'vue-router'
+import Index from "@/pages/frontend/index.vue"
+import { createRouter, createWebHashHistory } from "vue-router"
 
-import Login from '@/pages/admin/login.vue'
+import Login from "@/pages/admin/login.vue"
+import AdminIndex from "@/pages/admin/index.vue"
 
 const routes = [
-    {
-        path: '/', // 路由地址
-        component: Index, // 对应组件
-        meta: { // meta 信息
-            title: 'Weblog 首页' // 页面标题
-        }
-    },
-    {
-        path: '/login', // 登录页
-        component: Login,
-        meta: {
-            title: 'Weblog 登录页'
-        }
-    },
+	{
+		path: "/", // 路由地址
+		component: Index, // 对应组件
+		meta: {
+			// meta 信息
+			title: "Weblog 首页", // 页面标题
+		},
+	},
+	{
+		path: "/login", // 登录页
+		component: Login,
+		meta: {
+			title: "Weblog 登录页",
+		},
+	},
+	{
+		path: "/admin/index", // 后台首页
+		component: AdminIndex,
+		meta: {
+			title: "Admin 后台首页",
+		},
+	},
 ]
 
 // 创建路由
 const router = createRouter({
-    // 指定路由的历史管理方式，hash 模式指的是 URL 的路径是通过 hash 符号（#）进行标识
-    history: createWebHashHistory(),
-    // routes: routes 的缩写
-    routes, 
+	// 指定路由的历史管理方式，hash 模式指的是 URL 的路径是通过 hash 符号（#）进行标识
+	history: createWebHashHistory(),
+	// routes: routes 的缩写
+	routes,
 })
 
 // ES6 模块导出语句，它用于将 router 对象导出，以便其他文件可以导入和使用这个对象
