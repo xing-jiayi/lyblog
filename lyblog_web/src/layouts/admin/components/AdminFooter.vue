ded6c30caa9af9942ade57306e0a3751a4713d08
ayi <2294931964@qq.com>
2025-10-14 11:01:30 +0800

    组件过渡动画

--- lyblog_web/src/layouts/admin/components/AdminFooter.vue
+++ lyblog_web/src/layouts/admin/components/AdminFooter.vue
@@ -1,3 +1,7 @@
 <template>
-	<div class="bg-cyan-700 text-white">底部</div>
+  <div class="bg-white py-5 flex items-center justify-center text-sm text-gray-500 shadow-none">
+    <!-- Copyright 版权信息 -->
+    Copyright © 2025. All rights reserved. Provided by&nbsp; 
+    <a class="underline" href="https://www.crushtj.top" target="_blank">刑加一</a>
+  </div>
 </template>
